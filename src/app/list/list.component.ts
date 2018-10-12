154a6919cabd19af0c76138f1cb251ff601efe7e
Marc <marc.webdev@yahoo.com>
2018-10-12 13:02:10 +0200

    Rabobank Angular assignment: add jsdoc, remove unused code

--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -1,9 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 
-interface rowItem {
-  key: string; value: string;
-}
-
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
@@ -17,7 +13,6 @@ export class ListComponent implements OnInit {
   headers;
   rows;
   csvData;
-  displayCsvData;
   rowKeys;
   availableIssueCounts;
   csvLoaded;
@@ -27,21 +22,33 @@ export class ListComponent implements OnInit {
     this.nrOfHeaders = 4;
     this.nrOfRowsPerPage = 5;
 
+    // contains the name of the headersof the csv file
     this.headers = new Array(this.nrOfHeaders);
+    // contains the name of the headersof the csv file
     this.rows = new Array(this.nrOfRowsPerPage);
+    // contains normalized column names used as keys
     this.rowKeys = [];
+    // array to keep track which rows should not be visible
     this.dimmedRows = new Array(this.rows.length).fill(false);
 
-    this.displayCsvData = new Array(this.nrOfRowsPerPage).fill(new Array(this.nrOfHeaders));
     this.csvData;
 
+    // iterable needed to dynamically build options in select
+    // https://stackoverflow.com/questions/48393353/angular-ngfor-without-an-iterable-need-to-build-the-iterable
     this.availableIssueCounts = [1,2,3,4,5,6,7,8,9,10];
+    // needed to access csv data in template conditionally
     this.csvLoaded = false;
     }
 
   ngOnInit() {
   }
 
+  /**
+   * fileChangeListener
+   * @description: called when user selects a file from local fileSystem
+   * @param $event
+   * @return void 
+   */
   fileChangeListener($event): void {
     const input = $event.target;
     const fileReader = new FileReader();
@@ -56,22 +63,32 @@ export class ListComponent implements OnInit {
       // split first item of array on commas and convert item to stringarray - [[item1,item2,item3]]
       this.headers = dataArray[0]
         .split(csvStringDelimiter)
+        // remove double quote
         .map(header => header.replace(/"/g, ''))
         .map(header => {
+          // convert column names to snake case
           let column = header
             .replace(/\b[a-z]/g, (char => '_' + char.toUpperCase()))
             .replace(/\s/g, '');
+            // e.g. {key:'Sur_Name', value:'Marc Bakker'}
           return { key:column, value:header }
         });
+        // retrieve the columnkeys for easy access in stringArray2ObjectArray  
       this.rowKeys = this.headers.map(header => header.key);
       // split remaining items of array on commas and convert to stringarray  - [[item1,item2,item3],[...],...]
       this.csvData = this.stringArray2ObjectArray(dataArray.slice(1), this.rowKeys, csvStringDelimiter);
-      // we need deep copy that can be independently mutated 
-      this.displayCsvData = this.csvData.map(row => row.map(ob => ({...ob})));
+      // triggers template to display date
       this.csvLoaded = true;  
     }
   }
 
+  /**
+   * stringArray2ObjectArray
+   * @param {Array} stringArray: array of stringarrays containing strings - [[str1,str2,str3],...]
+   * @param {Array} keys: array of keys identifying column values
+   * @param {String} delimiter: the delimiter that delimits the items in the string arrays
+   * @return {Array} an object Array
+  */
   stringArray2ObjectArray(stringArray, keys, delimiter): string[][] {
     let i = 0;
 
@@ -86,20 +103,17 @@ export class ListComponent implements OnInit {
     });
   }
 
+  /**
+   * onChangeIssueCount
+   * @description callback for dropdown issueCount
+   * @param {number} newCount: number of issues
+   * @return void
+   * */
   onChangeIssueCount(newCount) {
+    // html input is string, convert to number
     newCount = parseInt(newCount);
     this.dimmedRows = this.csvData.map(row => row.some(rowItem => rowItem.key == 'Issue_Count' && parseInt(rowItem.value) < newCount))
-    this.dimmedRows = this.dimmedRows.concat(new Array(this.rows.length - this.displayCsvData.length).fill(false))    
-
-    // const filterByIssueCount = (rows: Array<rowItem[]>, issueCount: number): Array<rowItem[]> => {
-    //   return rows.filter(row => row
-    //     .some(rowItem => rowItem.key == 'Issue_Count' && parseInt(rowItem.value) < newCount)
-    //     )
-    // }
-
-    // this.displayCsvData = filterByIssueCount(this.csvData, newCount);
-
+    this.dimmedRows = this.dimmedRows.concat(new Array(this.rows.length - this.csvData.length).fill(false))    
   }
-
 
 }
